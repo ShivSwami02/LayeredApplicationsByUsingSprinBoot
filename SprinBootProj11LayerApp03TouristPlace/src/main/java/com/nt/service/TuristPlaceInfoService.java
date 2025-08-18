5528c61bb5dcacc3d2a9630fe6065980b1554a53
NCS-PC 4 <NCS-PC 4@Swami.mshome.net>
2025-08-18 12:02:59 +0530

    Third Application Layered App

---
+++ SprinBootProj11LayerApp03TouristPlace/src/main/java/com/nt/service/TuristPlaceInfoService.java
@@ -0,0 +1,49 @@
+package com.nt.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.nt.dao.ITouristPlaceDAO;
+import com.nt.model.TuristPlaceInfo;
+
+@Service("tpService")
+public class TuristPlaceInfoService implements ITouristPlaceInfoService{
+	@Autowired
+	@Qualifier("database")
+	private ITouristPlaceDAO dao;
+
+	@Override
+	public List<TuristPlaceInfo> getTuristPlaceInformationByLocation(String loc1, String loc2, String loc3)
+			throws Exception {
+		loc1=loc1.toUpperCase();
+		loc2=loc2.toUpperCase();
+		loc3.toUpperCase();
+		
+		try{
+			List<TuristPlaceInfo> list=dao.getTouristPlaceInformationByLocation(loc1, loc2, loc3);
+			return list;
+		}
+		catch(Exception e) {
+			throw e;
+		}
+		
+	}//method
+
+	@Override
+	public String getTuristPlaceInsertInfo(TuristPlaceInfo tp) throws Exception {
+		String msg=null;
+		try {
+			int count=dao.setTuristPlaceInfo(tp);
+			
+			msg=count==0?"Not Inserted!!!":"Inserted Successfully...";
+		}
+		catch(Exception e) {
+			throw e;
+		}
+		return msg;
+	}
+
+}//class
